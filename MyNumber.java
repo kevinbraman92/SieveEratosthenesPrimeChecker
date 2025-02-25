9ca75c0dec2a73856fa4c16e8ad797746e5427b4
kevinbraman92 <128657573+kevinbraman92@users.noreply.github.com>
2025-02-25 11:40:44 -0600

    Inital file upload.

---
+++ MyNumber.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyNumber {
+    private int number = 2;
+    private int countOfPrimes = 0;
+    private boolean[] booleanArray = new boolean[this.number + 1];
+    private ArrayList<Integer> primesList = new ArrayList<>();
+
+    MyNumber() {
+    }
+
+    MyNumber(int number) {
+        this.setNumber(number);
+    }
+
+    public void setNumber(int number) {
+        try {
+            if (number < 2) {
+                throw new IllegalArgumentException(
+                        "\nNumber argument cannot be less than 2! If creating a new object, the number data member will default to 2.");
+            } else {
+                this.number = number;
+                this.prepareArray();
+                this.sievePrimeMethod(this.booleanArray);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /*
+     * Creates a boolean array the size of the number + 1 to represent all integers.
+     * The array is filled by default to true, all numbers are considered prime by
+     * default.
+     * Index 0 and 1, representing numbers 0 and 1, are set to false as these
+     * numbers are never prime.
+     * The array is returned.
+     */
+    private boolean[] prepareArray() {
+        this.primesList.clear();
+        this.booleanArray = new boolean[this.number + 1];
+        Arrays.fill(booleanArray, true);
+        booleanArray[0] = booleanArray[1] = false;
+        return booleanArray;
+    }
+
+    /*
+     * The Sieve algrothim loops through the array starting at index position 2,
+     * finding all multiples up to the specified number.
+     * All multiples are marked as false. All remaning index positions that are true
+     * are prime numbers. Following the algorithim,
+     * the result array is looped through again to count all positions that are true
+     * and thus prime numbers. They are added to primesList array list.
+     */
+    private boolean[] sievePrimeMethod(boolean[] array) {
+        for (int i = 2; i * i <= this.number; i++) {
+            if (array[i]) {
+                for (int j = i * i; j <= this.number; j += i) {
+                    array[j] = false;
+                }
+            }
+        }
+
+        int primeCount = 0;
+        for (int i = 2; i <= this.number; i++) {
+            if (array[i]) {
+                primeCount++;
+                this.primesList.add(i);
+            }
+        }
+
+        this.countOfPrimes = primeCount;
+        return array;
+    }
+
+    public void printPrimeList() {
+        System.out.println(this.primesList);
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public Boolean isPrime() {
+        return this.booleanArray[this.number];
+    }
+
+    public int numberOfPrimes() {
+        return this.countOfPrimes;
+    }
+}
---
+++ UserInterface.java
@@ -0,0 +1,80 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInterface {
+    private MyNumber primeObject = new MyNumber();
+    private Scanner stringInput = new Scanner(System.in);
+    private Scanner integerInput = new Scanner(System.in);
+
+    UserInterface() {
+    }
+
+    private void welcomeMessage() {
+        System.out.println("\nWelcome to the prime number validator!");
+        System.out.println(
+                "This program can determine if a number is prime and the number of prime numbers that exist between 2 and the given integer.");
+        System.out.println("The program uses the Sieve of Eratosthenes algorithm to calculate the prime numbers.");
+        System.out.println("Enter 'Exit' to exit the program or 'Help' to see a list of commands.");
+    }
+
+    private void commands() {
+        System.out.println("\nThis program supports the following commands:");
+        System.out.println("Help: Will output this command list.");
+        System.out.println(
+                "Check Prime: Will ask for a number and check if it is prime as well as how many prime numbers exist from 2 to that number.");
+        System.out.println("Welcome: Will reprint the welcome message.");
+        System.out.println("Exit: Will exit the program.");
+    }
+
+    private void primeCheck(int userInteger) {
+        if (userInteger < 2) {
+            System.out.println(
+                    "User integer cannot be less than two, as 2 is the lowest prime number. Returning to main menu...");
+            return;
+        }
+
+        this.primeObject.setNumber(userInteger);
+        if (this.primeObject.isPrime()) {
+            System.out.println("\n" + userInteger + " is prime.");
+        } else {
+            System.out.println("\n" + userInteger + " is not prime.");
+        }
+        System.out.println(
+                "There are " + this.primeObject.numberOfPrimes() + " prime numbers between 2 and " + userInteger);
+        this.primeObject.printPrimeList();
+    }
+
+    private void userPrompt() {
+        this.welcomeMessage();
+        while (true) {
+            System.out.print("\nPlease enter a command: ");
+            String command = this.stringInput.nextLine();
+            if (command.equalsIgnoreCase("exit") | command.equalsIgnoreCase("quit")) {
+                System.out.println("Exiting program...");
+                System.exit(0);
+            } else if (command.equalsIgnoreCase("help") || command.equalsIgnoreCase("commands")) {
+                this.commands();
+            } else if (command.equalsIgnoreCase("welcome")) {
+                this.welcomeMessage();
+            } else if (command.equalsIgnoreCase("check prime") || command.equalsIgnoreCase("prime")) {
+                while (true) {
+                    try {
+                        System.out.print("\nPlease enter a number. The number must be 2 or greater: ");
+                        int userInteger = this.integerInput.nextInt();
+                        this.primeCheck(userInteger);
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("This command only accepts integer input. Please try again.");
+                        this.integerInput.next();
+                    }
+                }
+            } else {
+                System.out.println("\nCommand unknown.");
+            }
+        }
+    }
+
+    public void runInterface() {
+        this.userPrompt();
+    }
+}
